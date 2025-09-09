c90580dfdf480ed94e7cce203fc8804ebf560cfa
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-09-09 14:39:44 +0530

    add

---
+++ new/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Contact
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "short_message", "created_at")
+    search_fields = ("name", "email")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)   # newest first
+
+    def short_message(self, obj):
+        return obj.message[:50] + ("..." if len(obj.message) > 50 else "")
+    short_message.short_description = "Message"
+
---
+++ new/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render 
+from .forms import ContactForm 
+from .models import Contact
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html',{} )
+
+def home(request):
+    return render(request,'home.html',{})
+
+def contact_view(request):
+    if request.method == 'POST': 
+        form = ContactForm(request.POST) 
+        if form.is_valid():
+            form.save()
+            name = form.cleaned_data['name'] 
+            email = form.cleaned_data['email'] 
+            message = form.cleaned_data['message'] 
+        return render(request, 'success.html', {"name": name})
+    else: 
+        form = ContactForm() 
+    return render(request, 'contact.html', {"form": form})
