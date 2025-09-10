d5afe1a6a4af8a775a63b2fa397ca892f4199040
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-09-10 19:03:22 +0530

    changes_and_add

--- admin/admin.py
+++ admin/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+list_display = ('name', 'price', 'available', 'created_at')
+list_filter = ('available', 'created_at')
+prepopulated_fields = {'slug': ('name',)}
+search_fields = ('name', 'description')
--- admin/views.py
+++ admin/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
