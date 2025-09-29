57ebd7061a201443c6b8831bcdc0a64d60f7f395
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-09-29 10:09:35 +0530

    &&&__^^^^

--- new/views.py
+++ new/views.py
@@ -11,15 +11,18 @@ def home(request):
     return render(request,'home.html',{})
 
 def contact_view(request):
-    if request.method == 'POST': 
-        form = ContactForm(request.POST) 
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            name = form.cleaned_data['name'] 
-            email = form.cleaned_data['email'] 
-            message = form.cleaned_data['message'] 
-        return render(request, 'success.html', {"name": name})
-    else: 
-        form = ContactForm() 
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            return render(request, 'success.html', {"name": name})
+    else:
+        form = ContactForm()
+
+
     return render(request, 'contact.html', {"form": form})
+
 
