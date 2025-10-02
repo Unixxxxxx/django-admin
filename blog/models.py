db508a0fb1ca890873468442aeaab41c4ef8a7f2
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-10-02 11:46:15 +0530

    __^^

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)  # 👈 added image field
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
