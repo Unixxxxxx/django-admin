feaf1dfa4ff316929114dc5ae5aa1721ac4fbab6
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-10-10 13:56:21 +0530

    %%$

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.blog_list, name='blog_list'),
-    path('<int:pk>/', views.blog_detail, name='blog_detail'),
-]
-
