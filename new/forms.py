f87a47856db0f2403df0f02f0305e7004ac571ac
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-10-11 23:17:44 +0530

    __^^

--- new/forms.py
+++ new/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from .models import Contact, new
+from .models import Contact,new
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'message']
 
-class NewForm(forms.modelForm):
+class NewForm(forms.ModelForm):
     class Meta:
         model = new
-        field =[ 'name', 'message']
+        fields =[ 'name', 'message']
