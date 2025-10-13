6f27c36e46fbb916be9d6f151d9ef776f9525b2b
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-10-13 11:00:44 +0530

    __*&

--- app/urls.py
+++ app/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("grappelli/", include("grappelli.urls")),
     path('admin/', admin.site.urls),
-    path('',include('new.urls')),
+    path('new/',include('new.urls')),
 ]
 
 if settings.DEBUG:
