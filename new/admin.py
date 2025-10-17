6fd99c46e52390856750ebff646e002288d41136
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-10-17 15:11:22 +0530

    **

--- new/admin.py
+++ new/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Contact, new
+from .models import Contact, new, alpha 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ("name", "email", "short_message", "created_at")
     search_fields = ("name", "email")
     list_filter = ("created_at",)
-    ordering = ("-created_at",)   # newest first
+    ordering = ("-created_at",) 
 
     def short_message(self, obj):
         return obj.message[:50] + ("..." if len(obj.message) > 50 else "")
@@ -15,3 +15,5 @@ class ContactAdmin(admin.ModelAdmin):
 class NewAdmin(admin.ModelAdmin):
     list_display = ('name', 'message')  
     search_fields = ('name',)  
+
+
