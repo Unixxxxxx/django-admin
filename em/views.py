46c699a0d98261084611a7d57e0fa8bf0fe3ced3
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-10-23 08:55:04 +0530

    **

--- em/views.py
+++ em/views.py
@@ -5,8 +5,8 @@ def test_email(request):
     send_mail(
         subject='SMTP Test from Django',
         message='This is a test email from your Django app.',
-        from_email=None,  # Uses DEFAULT_FROM_EMAIL in settings.py
-        recipient_list=['your_email@gmail.com'],  # change to your email
+        from_email=None,  
+        recipient_list=['your_email@gmail.com'],
         fail_silently=False,
     )
     return HttpResponse("Test email sent successfully!")
